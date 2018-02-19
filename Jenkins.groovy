a664a83b95cd2519f8cb29449cf1386cfc9a4fc2
paras <parasgautam2011@gmail.com>
2018-02-19 15:19:40 -0600

    Update Jenkins.groovy

--- Jenkins.groovy
+++ Jenkins.groovy
@@ -1,7 +1,7 @@
 // Maven location
-def mvnHome = tool name: 'maven 3_5_0', type: 'hudson.tasks.Maven$MavenInstallation'
-env.MAVEN_HOME = M2_HOME
+//def mvnHome = tool name: 'maven 3_5_0', type: 'hudson.tasks.Maven$MavenInstallation'
+//env.MAVEN_HOME = M2_HOME
 
 // Begin Compile
 stage concurrency: 1, name: 'compile'
-sh '''${MAVEN_HOME}/bin/mvn clean compile'''
+sh '''$C:/Users/Paras/apache-maven-3.5.0/bin/mvn clean compile'''
