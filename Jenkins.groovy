d06dceb94a1a88e38600fae09f4e761aac8697ae
paras <parasgautam2011@gmail.com>
2018-02-19 15:30:19 -0600

    Update Jenkins.groovy

--- Jenkins.groovy
+++ Jenkins.groovy
@@ -1,19 +1,7 @@
-node{
-  stage ('Build') {
- 
-    //git url: 'https://github.com/cyrille-leclerc/multi-module-maven-project'
- 
-    withMaven(
-        // Maven installation declared in the Jenkins "Global Tool Configuration"
-        maven: 'M2',
-        // Maven settings.xml file defined with the Jenkins Config File Provider Plugin
-        // Maven settings and global settings can also be defined in Jenkins Global Tools Configuration
-        mavenSettingsConfig: 'my-maven-settings',
-        mavenLocalRepo: '.repository') {
- 
-      // Run the maven build
-      sh "mvn clean install"
- 
-    } // withMaven will discover the generated Maven artifacts, JUnit Surefire & FailSafe & FindBugs reports...
-  }
-}
+// Maven location
+def mvnHome = tool name: 'maven 3_3_9', type: 'hudson.tasks.Maven$MavenInstallation'
+env.MAVEN_HOME = mvnHome
+
+// Begin Compile
+stage concurrency: 1, name: 'compile'
+sh '''${MAVEN_HOME}/bin/mvn clean compile'''
