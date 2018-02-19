02e15efb4bd4d8a33fe0718433f58056a253c48e
paras <parasgautam2011@gmail.com>
2018-02-19 15:49:42 -0600

    Update Jenkins.groovy

--- Jenkins.groovy
+++ Jenkins.groovy
@@ -4,6 +4,6 @@ node {
   git url: 'https://github.com/parasgautam2011/springbootcrud.git'
   def mvnHome = tool 'MAVEN_HOME'
   sh "${mvnHome}/bin/mvn -B -Dmaven.test.failure.ignore verify"
- // step([$class: 'JUnitResultArchiver', testResults:
-//'**/target/foobar/TEST-*.xml'])
+  step([$class: 'JUnitResultArchiver', testResults:
+'**/target/foobar/TEST-*.xml'])
 }
